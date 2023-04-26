230495a612b055969c7f5b9cc8655c86de8cc77a
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-04-26 11:15:58 -0300

    exercicio 02 adicionado sobre arrays

---
+++ listas(arrays)/exercicio01-array.js
@@ -0,0 +1,10 @@
+//TERMINAL node '.\listas(arrays)\exercicio01-array.js'
+
+/*
+ 1) Crie um programa que dado o numero imprima a sua tabuada.
+*/
+
+const numero = 10;
+for (let i = 1; i <= 10; i++) {
+    console.log(i * numero);
+}
