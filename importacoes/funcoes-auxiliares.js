bfa959fa63fb039f2bc05ba470e1c327dddc261d
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-05-15 17:58:55 -0300

    exercício sobre importação e exportação

--- importacoes/funcoes-auxiliares.js
+++ importacoes/funcoes-auxiliares.js
@@ -1,7 +1,13 @@
 //TERMINAL: node .\importacoes\funcoes-auxiliares 
 
+const entradas = [10,5,50,10,98,23,90,69,99,21];
+let i = 0;
+
+
 function gets() {
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 function print(texto) {
     console.log(texto);
