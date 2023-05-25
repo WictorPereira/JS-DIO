a1304eaec8cd76d3172fee75bd37196233c006db
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2023-05-24 21:40:14 -0300

    tipos de typescript e funçoes

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,11 +1,61 @@
-type heroi = {
-    name: string;
-    vulgo: string;
+//salvar tudo : npm run start
+//Tipos primitivos:boolean, number, string
+let ligado:boolean = true;
+let nome:string = "wictor";
+let idade:number = 19;
+
+//Tipos especiais: null, undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+//Tipos abrangentes:any, void
+//any= recebe qualquer coisa/ void = nao retorna nada (vazio)
+let retorna:void;
+let retornoView:any = false
+
+//objeto - sem previsibilidade
+let produto: object = {
+    name:"wictor",
+    cidade:"RN",
+    idade:21,
 };
-function printaObjetos(pessoa: heroi){
-    console.log(pessoa);
+
+//objeto tipado - com previsibilidade
+type produtoLoja = {
+    nome:string;
+    preco:number;
+    unidades:number;
+};
+let meuProduto:produtoLoja = {
+    nome:"Ténis",
+    preco:100.00,
+    unidades:10,
+};
+
+//arrays
+let dados: string[] = ["wictor","joao","ana","felipe"];
+let dados2: Array<string> = ["wictor","joao","ana","felipe"];
+//arrays de multi Types
+let infos:(string | number)[] = ["wictor",30,"ana",19];
+
+//Tuplas (respeitar as ordens)
+let boleto:[string, number, number] = ["wictor",299.8,590];
+
+//arrays métodos 
+dados.map 
+
+//Datas
+let aniversario: Date = new Date("2023-08-08 18:00");
+console.log(aniversario.toString());
+
+//funções
+function addNumber(x:number,y:number): number {
+    return x + y;
 }
-printaObjetos({
-    name: "bruce wayne",
-    vulgo: "batman",
-});
+
+function addToHello(name:string): string{
+    return `Hello ${name}`;
+}
+let soma:number = addNumber (4, 7);
+console.log(soma);
+console.log(addToHello("Wictor"));
--- build/src/index.js
+++ build/src/index.js
@@ -1,8 +1,46 @@
 "use strict";
-function printaObjetos(pessoa) {
-    console.log(pessoa);
+//salvar tudo : npm run start
+//Tipos primitivos:boolean, number, string
+let ligado = true;
+let nome = "wictor";
+let idade = 19;
+//Tipos especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+//Tipos abrangentes:any, void
+//any= recebe qualquer coisa/ void = nao retorna nada (vazio)
+let retorna;
+let retornoView = false;
+//objeto - sem previsibilidade
+let produto = {
+    name: "wictor",
+    cidade: "RN",
+    idade: 21,
+};
+let meuProduto = {
+    nome: "Ténis",
+    preco: 100.00,
+    unidades: 10,
+};
+//arrays
+let dados = ["wictor", "joao", "ana", "felipe"];
+let dados2 = ["wictor", "joao", "ana", "felipe"];
+//arrays de multi Types
+let infos = ["wictor", 30, "ana", 19];
+//Tuplas (respeitar as ordens)
+let boleto = ["wictor", 299.8, 590];
+//arrays métodos 
+dados.map;
+//Datas
+let aniversario = new Date("2023-08-08 18:00");
+console.log(aniversario.toString());
+//funções
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjetos({
-    name: "bruce wayne",
-    vulgo: "batman",
-});
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+let soma = addNumber(4, 7);
+console.log(soma);
+console.log(addToHello("Wictor"));
